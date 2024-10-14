8e9ca6c6699dd4e74483f9db66fbae6bc0b7ee38
Shahbaz <shahbazathwal2107@gmail.com>
2024-10-14 13:37:50 -0300

    postman test checkpoint

--- socket-server/src/Game.ts
+++ socket-server/src/Game.ts
@@ -1,18 +1,66 @@
+import { BLACK, Chess, WHITE } from "chess.js";
 import { Socket } from "socket.io";
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
 
 export class Game {
-    private player1: Socket
-    private player2: Socket
-    private board: string
-    private moves: string[]
-    private startTime: Date
+  public player1: Socket;
+  public player2: Socket;
+  private board: Chess;
+  private moves: string[];
+  private startTime: Date;
 
+  constructor(player1: Socket, player2: Socket) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = new Chess();
+    this.moves = [];
+    this.startTime = new Date();
 
-    constructor(player1: Socket, player2: Socket) {
-        this.player1 = player1
-        this.player2 = player2
-        this.board = ""
-        this.moves = []
-        this.startTime = new Date()
+    // Send init_game to both players
+    this.player1.emit(INIT_GAME, { color: "white", startTime: this.startTime });
+    this.player2.emit(INIT_GAME, { color: "black", startTime: this.startTime });
+  }
+
+  makeMove(
+    socket: Socket,
+    move: {
+      from: string;
+      to: string;
     }
+  ) {
+    if (this.board.turn() === "w" && socket !== this.player1) {
+      return;
+    }
+
+    if (this.board.turn() === "b" && socket !== this.player2) {
+      return;
+    }
+
+    try {
+      this.board.move(move);
+      //   this.moves.push(move.from + move.to);
+
+      // Broadcast the move to both players
+      this.player1.emit(MOVE, move);
+      this.player2.emit(MOVE, move);
+    } catch (error) {
+      return;
+    }
+
+    // Check if the game is over
+    if (this.board.isGameOver()) {
+      const winner = this.board.turn() === WHITE ? BLACK : WHITE;
+
+      // Inform both players of game over
+      this.player1.emit(GAME_OVER, {
+        winner,
+      });
+
+      this.player2.emit(GAME_OVER, {
+        winner,
+      });
+
+      return;
+    }
+  }
 }
--- socket-server/src/GameManager.ts
+++ socket-server/src/GameManager.ts
@@ -1,45 +1,64 @@
-import { Socket } from "socket.io"
-import { INIT_GAME, MOVE } from "./messages"
-import { Game } from "./Game"
+import { Socket } from "socket.io";
+import { INIT_GAME, MOVE } from "./messages";
+import { Game } from "./Game";
 
 export class GameManager {
-    private games: Game[]
-    private pendingUser: Socket | null
-    private users: Socket[]
+  private games: Game[];
+  private pendingUser: Socket | null;
+  private users: Socket[];
 
-     
-    constructor() {
-        this.games = []
-        this.users = []
-        this.pendingUser = null
+  constructor() {
+    this.games = [];
+    this.users = [];
+    this.pendingUser = null;
+  }
+
+  addUser(socket: Socket) {
+    console.log("User Joined.");
+    this.users.push(socket);
+    this.addHandler(socket);
+
+    socket.on("disconnect", () => {
+      this.removeUser(socket);
+    });
+  }
+
+  removeUser(socket: Socket) {
+    // Remove the user from the list
+    this.users = this.users.filter(user => user !== socket);
+
+    // If the user was waiting to start a game, clear pendingUser
+    if (this.pendingUser === socket) {
+      this.pendingUser = null;
+      console.log("Pending user disconnected.");
     }
 
-    addUser(socket: Socket) {
-        this.users.push(socket)
-        this.addHandler(socket)
-    }
+    // Optionally, handle removing the user from any active games
+    this.games = this.games.filter(game => game.player1 !== socket && game.player2 !== socket);
+  }
 
-    removeUser(socket: Socket) {
-        this.users = this.users.filter(user => user !== socket)
-    }
+  private addHandler(socket: Socket) {
+    socket.on(INIT_GAME, () => {
+      console.log("Received INIT_GAME from a user.");
+      if (this.pendingUser) {
+        const game = new Game(this.pendingUser, socket);
+        console.log("Game Started.");
+        this.games.push(game);
+        this.pendingUser = null;
+      } else {
+        this.pendingUser = socket;
+        console.log("User is waiting for another player.");
+      }
+    });
 
-    private addHandler(socket: Socket) {
-        socket.on("message", (data) => {
-            const message = JSON.parse(data.toString())
-
-            if (message.type === INIT_GAME) {
-                if (this.pendingUser) {
-                    const game = new Game(this.pendingUser, socket)
-                    this.games.push(game)
-                    this.pendingUser = null
-                } else {
-                    this.pendingUser = socket
-                }
-            }
-
-            if (message.type === MOVE) {
-                
-            }
-        })
-    }
+    socket.on(MOVE, (moveData) => {
+        const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
+        if (game) {
+            game.makeMove(socket, moveData);
+            console.log("MOVE:", moveData);
+      } else {
+        console.log("No active game found for the user.");
+      }
+    });
+  }
 }
