08485e2fa810f91bfdd14ab03bf1610b3de4699e
Shahbaz <shahbazathwal2107@gmail.com>
2024-10-14 17:38:29 -0300

    cleanup

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
+import { Inter } from "next/font/google";
 import { type Metadata } from "next";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SocketChess",
@@ -13,7 +15,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en" className={`${inter.className}`}>
       <body>{children}</body>
     </html>
   );
