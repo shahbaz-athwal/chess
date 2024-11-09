0789ba8bfa8b281600fd2e5325f808b00bbdf5dd
Shahbaz <shahbazathwal2107@gmail.com>
2024-11-08 22:51:01 -0400

    navbar mobile view added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,12 +1,16 @@
-"use client";
-
+import { Navbar } from "@/components/Navbar";
 import { ThemeProvider } from "@/components/ui/theme-provider";
-import { ThemeToggle } from "@/components/ui/theme-toggle";
 import "@/styles/globals.css";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SocketChess",
+  description: "Chess games over WebSockets",
+};
 
 export default function RootLayout({
   children,
@@ -21,47 +25,14 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <div className="flex min-h-screen flex-col">
-            <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-              <div className="container mx-auto flex h-16 items-center justify-between px-4">
-                <div className="flex items-center space-x-6">
-                  <Link href="/" className="flex items-center space-x-2">
-                    <span className="sm:text-2xl text-lg font-bold">
-                      SocketChess
-                    </span>
-                  </Link>
-                  <nav className="hidden space-x-4 md:flex">
-                    <Link
-                      href="/game"
-                      className="text-sm font-medium transition-colors hover:text-primary"
-                    >
-                      Play
-                    </Link>
-                    <Link
-                      href="/current"
-                      className="text-sm font-medium transition-colors hover:text-primary"
-                    >
-                      Current Games
-                    </Link>
-                    <Link
-                      href="/about"
-                      className="text-sm font-medium transition-colors hover:text-primary"
-                    >
-                      About
-                    </Link>
-                  </nav>
-                </div>
-                <div className="flex items-center">
-                  <ThemeToggle />
-                </div>
-              </div>
-            </header>
+            <Navbar />
             <main className="flex-1">{children}</main>
             <footer className="border-t py-[1.32rem]">
               <div className="container mx-auto px-4 text-center text-sm text-muted-foreground">
                 <p>
                   Built by{" "}
                   <Link
-                    href="https://www.linkedin.com/in/shahbaz-athwal/"
+                    href="https://shahcodes.in"
                     target="_blank"
                     rel="noreferrer"
                     className="font-medium underline underline-offset-4 hover:text-primary"
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,72 @@
+import Link from "next/link"
+import { Button } from "./ui/button"
+import { Menu } from "lucide-react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
+import { ThemeToggle } from "./ui/theme-toggle"
+
+export function Navbar() {
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <div className="flex items-center space-x-12">
+          <Link href="/" className="flex items-center space-x-2">
+            <span className="text-xl font-bold sm:text-3xl">SocketChess</span>
+          </Link>
+          <nav className="hidden space-x-6 md:flex">
+            <Link
+              href="/game"
+              className="text-sm font-medium transition-colors hover:text-primary"
+            >
+              Play
+            </Link>
+            <Link
+              href="/current"
+              className="text-sm font-medium transition-colors hover:text-primary"
+            >
+              Current Games
+            </Link>
+            <Link
+              href="/about"
+              className="text-sm font-medium transition-colors hover:text-primary"
+            >
+              About
+            </Link>
+          </nav>
+        </div>
+        <div className="flex items-center space-x-4">
+          <ThemeToggle />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild className="md:hidden">
+              <Button variant="outline" size="icon">
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-[200px]">
+              <DropdownMenuItem asChild>
+                <Link href="/game" className="w-full">
+                  Play
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/current" className="w-full">
+                  Current Games
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/about" className="w-full">
+                  About
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </header>
+  )
+}
