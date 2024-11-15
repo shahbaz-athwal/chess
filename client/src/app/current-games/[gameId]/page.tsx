0063eba48077db526c81a52d5e915f4ba6448eb3
Shahbaz <shahbazathwal2107@gmail.com>
2024-11-15 14:12:14 -0400

    spectate test pass

--- client/src/app/current-games/[gameId]/page.tsx
+++ client/src/app/current-games/[gameId]/page.tsx
@@ -2,15 +2,26 @@
 
 import { useSocket } from "@/hooks/useSocket";
 import { useSpectate } from "@/hooks/useSpectate";
+import { useSpectateStore } from "@/hooks/useSpectateStore";
 
 function Spectate({ params }: { params: { gameId: string } }) {
   const gameId = params.gameId.replaceAll("%24", "$");
+
+  const { player1, player2, status, result, board, turn } = useSpectateStore();
 
   const { socket } = useSocket();
 
   useSpectate(socket, gameId);
 
-  return <div>gameId: {gameId}</div>;
+  return (
+    <>
+      <div>{player1} </div>
+      <div>{player2} </div>
+      <div>{status} </div>
+      <div>{result} </div>
+      <div>{turn} </div>
+    </>
+  );
 }
 
 export default Spectate;
